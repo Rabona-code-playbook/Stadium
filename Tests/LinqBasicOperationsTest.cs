cae9149788994cedeb000a7a17c6d9e22b278ade
José Luis Guzmán <cycopepe@gmail.com>
2023-08-02 20:18:30 -0500

    Add ProjectAndRename, SelectMany examples

--- Tests/LinqBasicOperationsTest.cs
+++ Tests/LinqBasicOperationsTest.cs
@@ -2,38 +2,41 @@ namespace Tests
 {
     public class LinqBasicOperationsTest
     {
-        public record Stadium {
+        public record Stadium
+        {
             public int Id { get; init; }
             public string Name { get; init; } = string.Empty;
             public string Country { get; init; } = string.Empty;
             public string Team { get; init; } = string.Empty;
+            public List<string> Colors { get; init; } = new List<string>();
         }
 
-        public record Country {
+        public record Country
+        {
             public string Name { get; init; } = string.Empty;
             public string Code { get; init; } = string.Empty;
         }
 
-        readonly List<string> stadiumNames = new() { "Wembley", "Camp Nou", "Santiago Bernabeu", "Old Trafford", "Maracana", "Allianz Arena", "Signal Iduna Park", "Stadio Giuseppe Meazza","Estadio Azteca", "Anfield", "Estadio Centenario", "La Bombonera", "Emirates" };
+        readonly List<string> stadiumNames = new() { "Wembley", "Camp Nou", "Santiago Bernabeu", "Old Trafford", "Maracana", "Allianz Arena", "Signal Iduna Park", "Stadio Giuseppe Meazza", "Estadio Azteca", "Anfield", "Estadio Centenario", "La Bombonera", "Emirates" };
 
         readonly List<Stadium> stadiums = new()
         {
             new Stadium{ Id = 1, Name = "Wembley", Country= "ENG", Team="England National Team" },
-            new Stadium{ Id = 2, Name = "Camp Nou", Country ="SPA", Team="Barcelona" },
-            new Stadium{ Id = 3, Name = "Santiago Bernabeu", Country ="SPA", Team="Real Madrid" },
+            new Stadium{ Id = 2, Name = "Camp Nou", Country ="SPA", Team="Barcelona", Colors = new List<string>{ "Blue", "Garnet" } },
+            new Stadium{ Id = 3, Name = "Santiago Bernabeu", Country ="SPA", Team="Real Madrid", Colors = new List<string>{ "White" } },
             new Stadium{ Id = 4, Name = "Old Trafford",  Country ="ENG", Team="Man U" },
             new Stadium{ Id = 5, Name = "Maracana",  Country ="BRA", Team="Brazil's National Team" },
             new Stadium{ Id = 6, Name = "Allianz Arena",  Country ="GER", Team="Bayern Munich" },
-            new Stadium{ Id = 7, Name = "Signal Iduna Park", Country ="GER", Team="Borusia Dortmund" },
-            new Stadium{ Id = 8, Name = "Stadio Giuseppe Meazza", Country ="ITA", Team="Inter, Milan" },
+            new Stadium{ Id = 7, Name = "Signal Iduna Park", Country ="GER", Team="Borusia Dortmund", Colors = new List<string>{ "Yellow", "Black" } },
+            new Stadium{ Id = 8, Name = "Stadio Giuseppe Meazza", Country ="ITA", Team="Inter, Milan", Colors = new List<string>{ "Red", "Blue" } },
             new Stadium{ Id = 9, Name = "Estadio Azteca",  Country ="MEX", Team="Mexico's National Team" },
-            new Stadium{ Id = 10, Name = "Anfield",  Country ="ENG", Team="Liverpool" },
+            new Stadium{ Id = 10, Name = "Anfield",  Country ="ENG", Team="Liverpool", Colors = new List<string>{ "Red" } },
             new Stadium{ Id = 11, Name = "Estadio Centenario", Country ="URU", Team="Peñarol, Uruguay's National Team" },
-            new Stadium{ Id = 12, Name = "La Bombonera", Country ="ARG", Team="Boca Juniors" },
-            new Stadium{ Id = 13, Name = "Emirates", Country ="ENG", Team="Arsenal" },
+            new Stadium{ Id = 12, Name = "La Bombonera", Country ="ARG", Team="Boca Juniors", Colors = new List<string>{ "Blue", "Yellow" } },
+            new Stadium{ Id = 13, Name = "Emirates", Country ="ENG", Team="Arsenal", Colors = new List<string>{ "Red", "White" } },
         };
 
-        readonly List<Country> countries = new () {
+        readonly List<Country> countries = new() {
             new Country{ Code = "ARG", Name  ="Argentina" },
             new Country{ Code = "BRA", Name = "Brazil" },
             new Country{ Code = "ENG", Name = "England" },
@@ -43,7 +46,7 @@ namespace Tests
         };
 
         [Fact]
-        public void ShouldFilter()
+        public void FilterString()
         {
             var filteredStadium = stadiumNames.Where(x => x.StartsWith("A")).ToList();
             Assert.True(filteredStadium.Count == 2); // Allianz Arena, Anfield
@@ -52,7 +55,7 @@ namespace Tests
         }
 
         [Fact]
-        public void ShouldFilterObjects()
+        public void FilterObjects()
         {
             var filteredStadium = stadiums.Where(x => x.Country.Equals("BRA")).ToList();
             Assert.True(filteredStadium.Count == 1); // Marcana
@@ -60,10 +63,11 @@ namespace Tests
         }
 
         [Fact]
-        public void ShouldJoinByCountry() {
-            var joined = stadiums.Join(countries, 
-                stadium => stadium.Country, 
-                country => country.Code, 
+        public void JoiningBySingleAttribute()
+        {
+            var joined = stadiums.Join(countries,
+                stadium => stadium.Country,
+                country => country.Code,
                 (stadium, country) => new { StadiumName = stadium.Name, FullCountryName = country.Name });
 
             Assert.True(joined.First().StadiumName.Equals("Wembley"));
@@ -71,10 +75,26 @@ namespace Tests
         }
 
         [Fact]
-        public void ShouldProject() {
-            var ids = stadiums.Select(x => new { x.Id }).ToList();
+        public void ProjectAndRename()
+        {
+            var ids = stadiums.Select(x => new { NewFieldContainingId = x.Id }).ToList();
 
-            Assert.True(ids[0].Id == 1);
+            Assert.True(ids[0].NewFieldContainingId == 1);
+        }
+
+        [Fact]
+        public void SelectMany()
+        {
+            var projected = stadiums.Select(x => x.Colors).ToList();
+            Assert.Equal(13, projected.Count);
+            Assert.Equal(new List<string>() { }, projected[0]);
+            Assert.Equal(new List<string>() { "Blue", "Garnet" }, projected[1]);
+
+            var flat = stadiums.SelectMany(x => x.Colors).ToList();
+
+            Assert.Equal(12, flat.Count);
+            Assert.Equal("Blue", flat.First());
+            Assert.Equal(new List<string>() { "Blue", "Garnet", "White", "Yellow", "Black", "Red", "Blue", "Red", "Blue", "Yellow", "Red", "White" }, flat);
         }
 
         /*
