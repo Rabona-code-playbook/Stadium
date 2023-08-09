6ac752872750ba9de59120a76af6a0a3c29b6c3f
José Luis Guzmán <cycopepe@gmail.com>
2023-08-09 16:12:21 -0500

    Add console DI example

--- StadiumConsole/Program.cs
+++ StadiumConsole/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace StadiumConsole;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace StadiumConsole;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine($"Hello, World! {args}");
+        // Need to add Microsoft.Extensions.Hosting nuget package
+        HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+        // register dependency in service container
+        builder.Services.AddSingleton<IPerson, Player>();
+        //  builder.Services.AddSingleton<IPerson, Coach>();
+
+        using IHost host = builder.Build();
+
+        // get the built-in service container
+        using IServiceScope serviceScope = host.Services.CreateScope();
+        // get the service provider from the service container
+        IServiceProvider serviceProvider = serviceScope.ServiceProvider;
+        // get the registerd service from the interface
+        IPerson person = serviceProvider.GetRequiredService<IPerson>();
+
+        person.HowIam();
+    }
+
+    public interface IPerson { 
+        void HowIam();
+    }
+
+    public class Player : IPerson
+    {
+        public void HowIam() => Console.WriteLine($"I am a player");
+    }
+
+    public class Coach : IPerson
+    {
+        public void HowIam() => Console.WriteLine($"I am a coach");
     }
 }
